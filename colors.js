cb9ac427d19a745aa19537216d0b1caefbb104d5
younseyeon <97429929+younseyeon@users.noreply.github.com>
2022-02-06 10:20:24 +0900

    생활코딩 HTML / CSS / JAVASCRIPT

---
+++ colors.js
@@ -0,0 +1,37 @@
+var Links = {
+  setColor: function (color) {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length) {
+    alist[i].style.color = 'color';
+    i = i + 1;
+  }
+}
+
+var Body = {
+setColor:function (color) {
+    document.querySelector('body').style.color = color;
+} ,
+SetBackgroundColor:function (color) {
+  document.querySelector('body').style.backgroundColor = color;
+}
+}
+
+function nightDayHandler(self){
+  var target = document.querySelector('body');
+  if(self.value === 'night') {
+    Body.SetBackgroundColor('black');
+    Body.SetColor('white');
+    self.value = 'day';
+
+    Links.SetColor('yellow');
+  }
+}  else {
+    Body.SetBackgroundColor('white');
+    Body.SetColor('black');
+    self.value = 'night';
+
+    Links.SetColor('blue')
+  }
+}
+}
